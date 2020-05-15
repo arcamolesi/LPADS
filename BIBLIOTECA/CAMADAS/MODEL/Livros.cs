aa415538202521cc9fa6c1b6dc6e55c87b82bfdc
Almir Camolesi <camolesi@fema.edu.br>
2020-05-14 22:00:34 -0300

    Classes DAL.Livros e MODEL.Livros

--- BIBLIOTECA/CAMADAS/MODEL/Livros.cs
+++ BIBLIOTECA/CAMADAS/MODEL/Livros.cs
@@ -8,5 +8,11 @@ namespace BIBLIOTECA.CAMADAS.MODEL
 {
     public class Livros
     {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string editora { get; set; }
+        public string autor { get; set; }
+        public float valor { get; set; }
+        public int situacao { get; set; }
     }
 }
