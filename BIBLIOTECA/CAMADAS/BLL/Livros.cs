1b34d1ba2ed1fea4fa27bd561a79ccd9f351ee7e
Almir Camolesi <camolesi@fema.edu.br>
2020-05-14 22:23:48 -0300

    BLL.Livros - OK

---
+++ BIBLIOTECA/CAMADAS/BLL/Livros.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIBLIOTECA.CAMADAS.BLL
+{
+    public class Livros
+    {
+        public List<MODEL.Livros> Select()
+        {
+            DAL.Livros dalLivro = new DAL.Livros();
+            ///... verificações e regras de negócios
+            return dalLivro.Select(); 
+        }
+
+        public void Insert (MODEL.Livros livro)
+        {
+            DAL.Livros dalLivro = new DAL.Livros();
+            // escrever regras de negócios
+
+            dalLivro.Insert(livro); 
+        }
+
+        public void Update (MODEL.Livros livro)
+        {
+            DAL.Livros dalLivro = new DAL.Livros();
+            //.regras de negócios
+            if (livro.autor!= String.Empty)
+                dalLivro.Update(livro); 
+        }
+
+        public void Delete(int idLivro)
+        {
+            DAL.Livros dalLivro = new DAL.Livros();
+            // regras de negócio 
+            if (idLivro > 0)
+               dalLivro.Delete(idLivro); 
+            
+        }
+    }
+}
---
+++ BIBLIOTECA/frmLivros.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace BIBLIOTECA
+{
+    partial class frmLivros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmLivros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 640);
+            this.Name = "frmLivros";
+            this.Text = "frmLivros";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
